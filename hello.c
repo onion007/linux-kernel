25c6eddd5320d0891c8df645662aaa1eab73e64c
Andy Ho <andy.ho@protegrity.com>
2016-11-28 15:12:22 +0800

    hello world for debian kernel

---
+++ hello.c
@@ -0,0 +1,17 @@
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/init.h>
+
+static int __init lkp_init(void)
+{
+    printk("<1> hello, world.\n");
+    return 0;
+}
+
+static void __exit lkp_cleanup(void)
+{
+    printk("<2> bye.\n");
+}
+
+module_init(lkp_init);
+module_exit(lkp_cleanup);
